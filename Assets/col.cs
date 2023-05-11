2111ea94ef7ace66790560742d5100835842e385
yuchen_huang <yuchenh9@illinois.edu>
2023-05-11 13:54:17 -0500

    commit

--- Assets/col.cs
+++ Assets/col.cs
@@ -7,13 +7,41 @@ namespace DynamicMeshCutter
 public class col : MonoBehaviour
 {
     // Start is called before the first frame update
+		string[] prefabnames;
+		Dictionary<string, GameObject> prefabDictionary;
 
 		public float TurnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
 		public float MoveSpeed = 4.0f;      // Speed of the camera going back and forth
-
+		private List<float> positions;
+		private List<GameObject> instances; 	
+		GameObject bowl;
+		GameObject prefab;
+		//(0,-0.432,2.225)
+		void addPreb(GameObject prefab){
+			var position = 2.722f;
+			if(positions.Count!=0){
+				position=positions[positions.Count-1]+0.6f;
+				//Debug.Log(position);
+			}
+			positions.Add(position);
+			GameObject instance = Instantiate(prefab, new Vector3(position,-0.432f,2.225f), Quaternion.Euler(-89.98f,0f,180f));
+            instances.Add(instance);
+		}
 		private float yaw = 0f;
 		private float pitch = 0f;
 		void Start(){
+			prefabDictionary = new Dictionary<string, GameObject>();
+			prefabnames=new string[] {"potato","carrot","tomato","cabbage","bell_pepper","steak","chicken_breast"};
+			positions = new List<float>();
+			instances = new List<GameObject>();
+			bowl = Resources.Load<GameObject>("bowl");
+			for (int i = 0; i < prefabnames.Length; i++)
+			{
+				prefab = Resources.Load<GameObject>(prefabnames[i]);
+				prefabDictionary.Add(prefabnames[i], prefab);
+				Debug.Log(i);
+				addPreb(prefab);
+			}
 			var roots = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
             foreach (GameObject root in roots)
 			{ 
@@ -52,7 +80,7 @@ public class col : MonoBehaviour
 			if (Mathf.Abs(move.sqrMagnitude) > Mathf.Epsilon)
 				camera.Translate(move, Space.Self);
 */
-			if (Input.GetKey(KeyCode.I))
+			if (Input.GetKey(KeyCode.LeftArrow))
             {    
 				Transform cameraTransform = Camera.main.gameObject.transform;
 
@@ -70,14 +98,17 @@ public class col : MonoBehaviour
 				cameraTransform.rotation = Quaternion.Euler((float)34.541, (float)0,(float)0);
 				Debug.Log("O");
 			}
-			if (Input.GetKey(KeyCode.P))
+			if (Input.GetKeyDown(KeyCode.L))
+            {    addPreb(bowl);
+			}
+			if (Input.GetKey(KeyCode.RightArrow))
             {    
 				Transform cameraTransform = Camera.main.gameObject.transform;
 
         // Set the position and rotation of the camera
 				cameraTransform.position = new Vector3((float)2.898, (float)0.746, (float)0.514);
 				cameraTransform.rotation = Quaternion.Euler((float)34.541, (float)0,(float)0);
-				Debug.Log("P");
+				//Debug.Log("P");
 			}
 			if (Input.GetMouseButton(2))
 			{
